11d3f41967acaf759a1411536298f866e65df6a3
hp <lars75@inbox.ru>
2023-04-13 23:38:01 +0300

    1. Создать элементарную структуру дерева [#1711]

---
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -0,0 +1,39 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+public class SimpleTree<E> implements Tree<E> {
+    private final Node<E> root;
+
+    public SimpleTree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean result = false;
+        Optional<Node<E>> opt = findBy(parent);
+        Optional<Node<E>> opt1 = findBy(child);
+        if (opt.isPresent() && opt1.isEmpty()) {
+            opt.get().children.add(new Node<>(child));
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+}
---
+++ src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,17 @@
+package ru.job4j.tree;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.List;
+
+public interface Tree<E> {
+    boolean add(E parent, E child);
+    Optional<Node<E>> findBy(E value);
+    class Node<E> {
+        final E value;
+        final List<Node<E>> children = new ArrayList<>();
+        public Node(E value) {
+            this.value = value;
+        }
+    }
+}
