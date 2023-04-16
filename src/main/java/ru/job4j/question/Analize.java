6b4ff2eeecf986ba698acc07f753605d73495b98
hp <lars75@inbox.ru>
2023-04-17 01:41:02 +0300

    3. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,37 @@
+package ru.job4j.question;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class Analize {
+    public static Info diff(Set<User> previous, Set<User> current) {
+        int added = 0;
+        int changed = 0;
+        int deleted = 0;
+        HashMap<Integer, String> mapPrevious = new HashMap<>();
+        for (var user : previous) {
+            mapPrevious.put(user.getId(), user.getName());
+        }
+
+        for (var user : current) {
+            if (!mapPrevious.containsValue(user.getName())
+                    && mapPrevious.containsKey(user.getId())) {
+                changed++;
+            }
+            if (mapPrevious.size() > current.size()) {
+                deleted++;
+                break;
+            }
+            if (mapPrevious.size() < current.size()) {
+                added++;
+                break;
+            }
+            if (!mapPrevious.containsKey(user.getId())
+                    && mapPrevious.size() == current.size()) {
+                added++;
+                deleted++;
+            }
+        }
+        return new Info(added, changed, deleted);
+    }
+}
---
+++ src/main/java/ru/job4j/question/User.java
@@ -0,0 +1,50 @@
+package ru.job4j.question;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + '}';
+    }
+}
