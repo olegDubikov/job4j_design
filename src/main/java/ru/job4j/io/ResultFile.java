79b1e090f826ea3f5f2681c3e6b4d5b53877b209
hp <lars75@inbox.ru>
2023-04-30 14:17:27 +0300

    2.0. FileOutputStream [#252488 #413081]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,31 +1,18 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
-            out.write(("1*1=1").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("1*2=2").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("1*3=3").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("1*4=4").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("1*5=5").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("1*6=6").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("1*7=7").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("1*8=8").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("1*9=9").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("1*10=20").getBytes());
-            out.write(System.lineSeparator().getBytes());
-        } catch (Exception e) {
+            var x = 1;
+            while (x <= 10) {
+                out.write(("1*" + x + "=" + x).getBytes());
+                out.write(System.lineSeparator().getBytes());
+                x++;
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
