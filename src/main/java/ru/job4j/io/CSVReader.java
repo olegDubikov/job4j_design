eb66f368aa1006346676295ed785131674c9872a
hp <lars75@inbox.ru>
2023-09-25 18:10:50 +0300

    17. Scanner [#504791 #447676]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -9,8 +9,9 @@ public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
         try (var scanner = new Scanner(new FileInputStream(argsName.get("path")))
                 .useDelimiter(argsName.get("delimiter"))) {
+            String delimiter = argsName.get("delimiter");
             String[] filters = argsName.get("filter").split(",");
-            String[] splitFilters = scanner.nextLine().split(argsName.get("delimiter"));
+            String[] splitFilters = scanner.nextLine().split(delimiter);
             String[] finishFilter = new String[splitFilters.length];
             for (var i = 0; i < filters.length; i++) {
                 for (String splitFilter : splitFilters) {
@@ -21,10 +22,10 @@ public class CSVReader {
                 }
             }
             List<String> filterCSV = new ArrayList<>();
-            filterCSV.add(argsName.get("filter").replace(",", argsName.get("delimiter")));
+            filterCSV.add(argsName.get("filter").replace(",", delimiter));
             while (scanner.hasNextLine()) {
-                String[] column = scanner.nextLine().split(argsName.get("delimiter"));
-                StringJoiner everyString = new StringJoiner(argsName.get("delimiter"));
+                String[] column = scanner.nextLine().split(delimiter);
+                StringJoiner everyString = new StringJoiner(delimiter);
                 for (var i = 0; i < column.length; i++) {
                     if (finishFilter[i] != null) {
                         int index = Arrays.asList(splitFilters).indexOf(finishFilter[i]);
@@ -34,7 +35,7 @@ public class CSVReader {
                 }
                 filterCSV.add(everyString.toString());
             }
-            if (argsName.get("out").equals("stdout")) {
+            if ("stdout".equals(argsName.get("out"))) {
                 printResult(filterCSV);
             } else {
                 printInFile(filterCSV, argsName);
@@ -61,13 +62,10 @@ public class CSVReader {
         if (!(Files.exists(Path.of(argsName.get("path"))) || argsName.get("path").endsWith(".csv"))) {
             throw new IllegalArgumentException("Error: Файл не существует или имеет неверное расширение");
         }
-        if (args.length != 4) {
-            throw new IllegalArgumentException("Error: В программу должно быть передано 4 аргумента");
-        }
-        if (!(args[1].endsWith(",") || args[1].endsWith(";"))) {
+        if (!argsName.get("delimiter").equals(";") && !argsName.get("delimiter").equals(",")) {
             throw new IllegalArgumentException("Error: Разделителем может быть запятая или точка с запятой");
         }
-        if (!(argsName.get("out").equals("stdout") || argsName.get("out").endsWith(".csv"))) {
+        if (!("stdout".equals(argsName.get("out")) || argsName.get("out").endsWith(".csv"))) {
             throw new IllegalArgumentException("Error: Некорректно введены аргументы вывода результата");
         }
         if (argsName.get("filter").endsWith("=")) {
@@ -77,7 +75,9 @@ public class CSVReader {
     }
 
     public static void main(String[] args) throws Exception {
-        ArgsName argsName = validateArgs(args);
-        handle(argsName);
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Error: В программу должно быть передано 4 аргумента");
+        }
+        handle(validateArgs(args));
     }
 }
