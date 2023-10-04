d16d7493427d4ac775fdb3150d4f62be6c3542e6
hp <lars75@inbox.ru>
2023-10-04 14:01:43 +0300

    1. Что такое Сериализация? [#313163]

---
+++ src/main/java/serialization/Contact.java
@@ -0,0 +1,65 @@
+package serialization;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Objects;
+
+public class Contact implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final int zipCode;
+    private final String phone;
+
+    public Contact(int zipCode, String phone) {
+        this.zipCode = zipCode;
+        this.phone = phone;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Contact contact = (Contact) o;
+        return zipCode == contact.zipCode && Objects.equals(phone, contact.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(zipCode, phone);
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{"
+                + "zipCode=" + zipCode
+                + ", phone='" + phone + '\''
+                + '}';
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        final Contact contact = new Contact(12345, "+7 (111) 111-11-11");
+        File tempFile = Files.createTempFile(null, null).toFile();
+        try (FileOutputStream fos = new FileOutputStream(tempFile);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(contact);
+        }
+        try (FileInputStream fis = new FileInputStream(tempFile);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            final Contact contactFromFile = (Contact) ois.readObject();
+            System.out.println(contactFromFile);
+            System.out.println(contact.equals(contactFromFile));
+        }
+
+    }
+}
