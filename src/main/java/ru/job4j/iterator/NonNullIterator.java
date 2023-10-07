731e9731cc47ba0216af141d73f9ede9729e1239
олегДубиков <lars75@inbox.ru>
2023-10-07 16:31:16 +0300

    5. Итератор не-null значений [#505081 #450591]

--- src/main/java/ru/job4j/iterator/NonNullIterator.java
+++ src/main/java/ru/job4j/iterator/NonNullIterator.java
@@ -14,15 +14,10 @@ public class NonNullIterator implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        boolean rsl = false;
-        while (index < data.length) {
-            if (data[index] != null) {
-                rsl = true;
-                break;
-            }
+        while (index < data.length && data[index] % 2 != 0) {
             index++;
         }
-        return rsl;
+        return index < data.length;
     }
 
     @Override
