1caf4225388d7aefb59213d72466cd0194c93199
олегДубиков <lars75@inbox.ru>
2023-11-05 21:04:17 +0300

    6. Циклический итератор [#505082]

---
+++ src/main/java/ru/job4j/iterator/CyclicIterator.java
@@ -0,0 +1,29 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class CyclicIterator<T> implements Iterator<T> {
+    private final List<T> data;
+    private int currentIndex = 0;
+
+    public CyclicIterator(List<T> data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !data.isEmpty();
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        T result = data.get(currentIndex);
+        currentIndex = (currentIndex + 1) % data.size();
+        return result;
+    }
+}
