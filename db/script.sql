70036875b4e593261779e5de6073485e6eeacb1e
олегДубиков <lars75@inbox.ru>
2023-11-13 20:50:22 +0300

    0. Что такое база данных. [#6860]

---
+++ db/script.sql
@@ -0,0 +1,9 @@
+﻿create table car(
+id serial primary key,
+model varchar(255),
+color text,
+power integer
+);
+insert into car(model, color, power) values('JMC', 'silver', 350);
+update car set color = 'yellow';
+delete from car
