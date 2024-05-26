07e204e0d245da635881540e4f085b2f5d88f9d7
олегДубиков <lars75@inbox.ru>
2024-05-26 18:45:26 +0300

    1. Реализация кеша на SoftReference [#1592 #497997]

--- src/main/java/ru/job4j/cache/menu/Emulator.java
+++ src/main/java/ru/job4j/cache/menu/Emulator.java
@@ -8,12 +8,12 @@ import java.util.Scanner;
 
 public class Emulator {
 
-    private static final Scanner scr = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
 
 
     public static void main(String[] args) {
         System.out.println("Выбирете директорию: ");
-        String dirPath = scr.nextLine();
+        String dirPath = sc.nextLine();
         DirFileCache dirFileCache = new DirFileCache(dirPath);
 
         boolean exit = false;
@@ -21,11 +21,11 @@ public class Emulator {
             System.out.println("Введите 1. Чтобы получить кеш файла");
             System.out.println("Введите 2. Для выхода из программы");
 
-            int choice = Integer.parseInt(scr.nextLine());
+            int choice = Integer.parseInt(sc.nextLine());
             switch (choice) {
                 case 1 -> {
                     System.out.println("Введите имя файла с расширением:");
-                    String fileName = scr.nextLine();
+                    String fileName = sc.nextLine();
                     String content = dirFileCache.get(fileName);
                     if (Files.isDirectory(Path.of(dirPath))) {
                         System.out.println(content);
@@ -33,7 +33,9 @@ public class Emulator {
                         throw new IllegalArgumentException(String.format("%s нет такой директории", dirPath));
                     }
                 }
-                case 2 -> exit = true;
+                case 2 -> {
+                    exit = true;
+                }
                 default -> System.out.println("Выбирайте 1 или 2");
             }
         }
